d323015b184f5bc18d0e3af645b040b960469a31
Nicolas Duthou <nicodami19@gmail.com>
2021-12-28 12:54:10 +0100

    redid ex4 to match performance checks: success!

--- ex4.py
+++ ex4.py
@@ -7,19 +7,24 @@ majuscules = 'abcdefghijklmnopqrstuvwxyz'.upper()
 nombres = '1234567890'
 speciaux = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
 
+decoded = ''
+keys = 'mMns'
+for char in chaine:
+    if char in minuscules:
+        decoded += 'm'
+    elif char in majuscules:
+        decoded += 'M'
+    elif char in nombres:
+        decoded += 'n'
+    else:
+        decoded += 's'
+
 possible_combinations = 0
-for i in range(0,len(chaine)-taille_mdp+1):
-    (min, maj, nb, sp) = (False, False, False, False)
-    for j in range(i,i+taille_mdp):
-        if chaine[j] in minuscules:
-            min = True
-        if chaine[j] in majuscules:
-            maj = True
-        if chaine[j] in nombres:
-            nb = True
-        if chaine[j] in speciaux:
-            sp = True
-    if min and maj and nb and sp:
-        possible_combinations += 1
+for i in range(0,len(decoded)-taille_mdp+1):
+    val_count = 1
+    for item in keys:
+        if item not in decoded[i:i+taille_mdp]:
+            val_count = 0
+    possible_combinations += val_count
 
 print(possible_combinations)
